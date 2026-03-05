18dae91ccb83b8354bf3a7aad4fdd6aeedc68bfe
vamsi <vamsikrishna.rangana12@gmail.com>
2026-03-05 09:13:01 +0530

    adding python files

---
+++ rm-spaces.py
@@ -0,0 +1,7 @@
+paragraph = """This is a sample paragraph with multiple spaces.
+It shows how to remove all spaces from text."""
+
+# Remove all space characters
+result = paragraph.replace(" ", "")
+
+print(result)
